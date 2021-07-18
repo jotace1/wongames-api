19aa12e840d538d6ac08f7e567df2a15d015aea6
jotace1 <joaocsc01@hotmail.com>
2021-07-18 12:15:33 -0300

    feat: started strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f6bd418a7154c6bd58b4a3ef473145b6'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e5eb7592-aff4-489a-95c6-3c7f5095c952'
+};
